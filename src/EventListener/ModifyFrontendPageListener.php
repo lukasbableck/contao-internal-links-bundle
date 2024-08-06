77986750406b6db6e6dae8a58a6fd426bff15fff
Lukas Bableck <lukas@bableck.dev>
2024-08-06 14:54:51 +0200

    add index model, replace keywords in ModifyFrontendPageListener

--- src/EventListener/ModifyFrontendPageListener.php
+++ src/EventListener/ModifyFrontendPageListener.php
@@ -3,12 +3,30 @@ namespace Lukasbableck\ContaoInternalLinksBundle\EventListener;
 
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\PageModel;
+use Contao\StringUtil;
+use Lukasbableck\ContaoInternalLinksBundle\Models\InternalLinkIndexModel;
 
 #[AsHook('modifyFrontendPage')]
 class ModifyFrontendPageListener {
 	public function __invoke(string $buffer, string $templateName): string {
 		$rootPage = PageModel::findByPk($GLOBALS['objPage']->rootId);
-		
+		$index = InternalLinkIndexModel::findBy(['rootPageID=?'], [$rootPage->id]);
+		if (!$index) {
+			return $buffer;
+		}
+
+		$keywords = [];
+		foreach ($index as $entry) {
+			$kw = StringUtil::deserialize($entry->keywords);
+			foreach ($kw as $keyword) {
+				$keywords[$keyword] = $entry->url;
+			}
+		}
+
+		$buffer = preg_replace_callback('/\b(' . implode('|', array_map('preg_quote', array_keys($keywords))) . ')\b/', function($matches) use ($keywords) {
+			return sprintf('<a href="%s">%s</a>', $keywords[$matches[0]], $matches[0]);
+		}, $buffer);
+
 		return $buffer;
 	}
 }
---
+++ src/Models/InternalLinkIndexModel.php
@@ -0,0 +1,8 @@
+<?php
+namespace Lukasbableck\ContaoInternalLinksBundle\Models;
+
+use Contao\Model;
+
+class InternalLinkIndexModel extends Model {
+	protected static $strTable = 'tl_internal_link_index';
+}
