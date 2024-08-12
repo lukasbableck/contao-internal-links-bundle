498f2858afa678ac2cb217462ad910be93553ee4
Lukas Bableck <lukas@bableck.dev>
2024-08-12 07:14:06 +0200

    this has to go in config.php

--- contao/config/config.php
+++ contao/config/config.php
@@ -3,3 +3,5 @@
 use Lukasbableck\ContaoInternalLinksBundle\Models\InternalLinkIndexModel;
 
 $GLOBALS['TL_MODELS']['tl_internal_link_index'] = InternalLinkIndexModel::class;
+
+$GLOBALS['TL_CONFIG']['internalLinkIgnoreElements'] = '<h1><h2><h3><h4><h5><h6><a><button><header><footer><script><style>';
--- contao/config/default.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$GLOBALS['TL_CONFIG']['internalLinkIgnoreElements'] = '<h1><h2><h3><h4><h5><h6><a><button><header><footer><script><style>';
