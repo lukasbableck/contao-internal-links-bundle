ea9df6ff49a63f83f2fb7d213b3b109a3f6ef23a
Lukas Bableck <lukas@bableck.dev>
2024-12-04 15:37:46 +0100

    add default values for tl_settings fields

--- contao/config/config.php
+++ contao/config/config.php
@@ -5,3 +5,5 @@ use Lukasbableck\ContaoInternalLinksBundle\Models\InternalLinkIndexModel;
 $GLOBALS['TL_MODELS']['tl_internal_link_index'] = InternalLinkIndexModel::class;
 
 $GLOBALS['TL_CONFIG']['internalLinkIgnoreElements'] = '<h1><h2><h3><h4><h5><h6><a><button><header><footer><script><style>';
+$GLOBALS['TL_CONFIG']['internalLinkCaseSensitive'] = false;
+$GLOBALS['TL_CONFIG']['internalLinkOccurrence'] = 'all';
